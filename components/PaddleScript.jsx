0042a09a27434338122e0804a3f40ce81a3f1bc6
Your Actual Name <your.actual.email@example.com>
2025-08-27 15:10:59 +0600

    fixed issues

--- components/PaddleScript.jsx
+++ components/PaddleScript.jsx
@@ -3,20 +3,15 @@ import Script from "next/script";
 import config from "../services/config";
 
 export default function PaddleScript() {
- 
 
   return (
-    <>
-      <Script src="https://cdn.paddle.com/paddle/paddle.js" strategy="afterInteractive" />
-      <Script
-        id="paddle-setup"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            Paddle.Setup({ vendor: ${config.paddleVendorId} });
-          `,
-        }}
-      />
-    </>
+    <Script
+    src="https://cdn.paddle.com/paddle/paddle.js"
+    onLoad={() => {
+      Paddle.Setup({
+        vendor: Number(config.paddleVendorId),
+      });
+    }}
+  />
   );
 }
